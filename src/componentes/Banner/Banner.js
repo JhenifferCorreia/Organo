3c72194f2123141941ec71fa9e9da25b0434d04a
Jhen <62966367+JhenifferCorreia@users.noreply.github.com>
2023-02-18 16:14:48 -0300

    Add files via upload

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react'
+import './Banner.css'
+
+export const Banner = () => {
+    return (
+        <>
+            <header className="banner">
+                <img src="./imagens/banner.png" alt="O banner principal da página Organo" />
+            </header>
+        </> //Fragment JSX 
+    )
+}
